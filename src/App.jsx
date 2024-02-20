4de2c4bc15fd3233d5c15ca8f80a1accbd610ab7
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2024-02-20 16:50:07 -0300

    Trabaje en la estructura de la seccion principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { Navbar } from './components/Navbar'
 
 function App () {
   return (
     <>
+      <Navbar />
+      <section className='grid grid-cols-2 h-screen items-center'>
+        <aside className=''>
+          <img src='https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg' alt='' />
+        </aside>
+        <main className='flex justify-center'>
+          <h1>A.I.L.A.A.C.C. U.E.P. 195</h1>
+        </main>
+      </section>
 
-      <h1 className='text-3xl font-bold underline'>
-        Hello world!
-      </h1>
+      <section id='services' className='h-screen items-center'>
+        <h1 />
+      </section>
 
     </>
   )
