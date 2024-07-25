cca08299729f7e6ba0fb068976a0d0d664adaa6a
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2024-07-25 11:36:03 -0300

    is_staff required

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-function PrivateRoutes ({ isAuthenticated }) {
+function PrivateRoutes ({ isAuthenticated, isStaff }) {
   return (
-    isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+    isAuthenticated && isStaff ? <Outlet /> : <Navigate to='/login' />
   )
 }
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
+  isStaff: state.auth.user ? state.auth.user.is_staff : false
 })
 
 export default connect(mapStateToProps)(PrivateRoutes)
