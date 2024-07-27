7d2185cdf71f1e8c54a9b81011701ed5a88ef06f
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2024-07-27 08:38:33 -0300

    user load fixed

--- src/hocs/Layout.jsx
+++ src/hocs/Layout.jsx
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { Footer } from '../components/Footer'
 import { connect } from 'react-redux'
 import { checkAuthenticated, loadUser } from '../actions/auth'
+import { Spinner } from '../components/ui/Spinner'
 
 function Layout (props) {
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    props.checkAuthenticated()
-    props.loadUser()
-  }, [])
+    async function loadAuthData () {
+      await props.checkAuthenticated()
+      await props.loadUser()
+      setLoading(false)
+    }
+    loadAuthData()
+  }, [props.checkAuthenticated, props.loadUser])
+
+  if (loading) {
+    return (
+      <div className='h-screen flex flex-col items-center justify-center'>
+        <Spinner />
+      </div>
+    )
+  }
 
   return (
     <div>
 
       <Navbar />
+      <div className=''>
 
-      {props.children}
+        {props.children}
+      </div>
       <Footer />
     </div>
   )
